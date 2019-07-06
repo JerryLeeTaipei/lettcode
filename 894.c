ca3708738f02400b75abd54cd445fa7c35a3d873
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-06 13:57:10 +0800

    Update 894.c

--- 894.c
+++ 894.c
@@ -1,19 +1,41 @@
+#include<stdio.h>
+
 /**
  * Definition for a binary tree node.
  */
 
 struct TreeNode {
+     int index;
      int val;
      struct TreeNode *left;
      struct TreeNode *right;
 };
 
 
-struct TreeNode* createNode() {
+struct TreeNode* createNode(int index) {
     struct TreeNode* tmpNode=NULL;
     tmpNode = malloc(sizeof(struct TreeNode));
+    tmpNode->val = index;
     tmpNode->val = 0;
-    return tmpNode;
+}
+
+int createChildren(struct TreeNode* node) {
+    struct TreeNode* tmpNode=NULL;
+    
+    tmpNode = malloc(sizeof(struct TreeNode));
+    if ( !tmpNode )
+        return 1;
+    tmpNode->val = index + 1;
+    tmpNode->val = 0;
+    node->left = tmpNode;
+    
+    tmpNode = malloc(sizeof(struct TreeNode));
+    if ( !tmpNode )
+        return 1;    
+    tmpNode->val = index + 2;
+    tmpNode->val = 0;
+    node->right = tmpNode;
+    return 0;
 }
 
 /*
@@ -28,13 +50,32 @@ struct TreeNode** allPossibleFBT(int N, int* returnSize){
 
     struct TreeNode** btList=NULL, newNode=NULL;
     int n=N;
-    while ( n > 0 ) {
-        newNode = createNode();
+    int index = 0;
+    int ret=0;
+    // build a complete binary tree with the depth (N+1)/2
+   
+    while ( n > 0 ) {       
+        newNode = createNode(index);
         if ( !newNode ) {
+            printf("Out of memory\n");
+            return NULL;
+        }           
+        n--;
+        index++;
+        if ( createChildren(newNode) != 0 ) {
+            printf("Failed to create children\n");
             break;
-        }
-        
-    }
+        }           
+        if ( !btList )
+            btList = newNode;
+        n -=2 ;
+        index += 2;
+    }    
+
     return btList;
 }
 
+int main(void) {
+    puts("Hello World!");
+    return 0;
+}
