fb21807811b1a893ba8eb121154b3f4ce4d7e45a
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-08 10:36:33 +0800

    Update 2.c

--- 2.c
+++ 2.c
@@ -2,36 +2,49 @@
  * Definition for singly-linked list.
   */
 
- struct ListNode {
+/**
+ * Definition for singly-linked list.
+struct ListNode {
       int val;
       struct ListNode *next;
- };
+ };  
+ */
+
 
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
-  int val=0, match=0;
+  int val=0, match=0, carry=0;
   struct ListNode *tmp=NULL, *tmp1=NULL, *tmp2=NULL, *result=NULL, *tail=NULL;
   tmp1 = l1;
   tmp2 = l2;
   while ( 1 ){
-    val = 0;
-    if ( tmp1 )
-      val += tmp1->val;
-      tmp1 = tmp1->next;
-    if ( tmp2 )
-      val += tmp2->val;
-      tmp2 = tmp2->next;
-    if ( !tmp1 && !tmp2 ) {
+    if ( !tmp1 && !tmp2 && !carry ) {
       break;
     }
+    val = carry;      
+    if ( tmp1 ) {
+      val += tmp1->val;
+      tmp1 = tmp1->next;
+    }
+    if ( tmp2 ) {
+      val += tmp2->val;
+      tmp2 = tmp2->next;
+    }
+    if ( val > 9 ) {
+        carry = 1;
+        val -= 10;
+    } else
+        carry = 0;
     tmp = malloc(sizeof(struct ListNode));
     tmp->val = val;
     tmp->next = NULL;
-    if ( !tail ) {// NOT 1st node
-      tail->next = tmp;
-      tail = tmp;
-    } else // 1st node
-      result = tail;
+    if ( !result ) {//  1st node
+        result = tmp;
+    } else { // append node
+        tail->next = tmp;
+    }
+    tail = tmp;
+
   }
   return result;
 }
