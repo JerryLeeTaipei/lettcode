7e208b45ad55200c47f2867477446555050202a7
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-09 03:51:18 +0800

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,24 +1,36 @@
+
+static char *ascii=NULL;
+
+
 int nonRepeatedLen(char *start){
     char *tmp=NULL;
-    char ascii[255];
-    
-    while (tmp) {
-        
+    int len=0;
+     
+    tmp = start;
+    while ( *tmp != 0 ) {
+        if ( ascii[*tmp] == 0 ) {
+            ascii[*tmp] = 1;
+            len++;
+        } else
+            break; 
         tmp++;
     }
+    return len;
 }
 
 int lengthOfLongestSubstring(char * s){
     int longest=0, len=0;
     char *tmp=NULL;
-    
+    ascii = malloc(sizeof(char) *255);
     tmp = s;
-    while ( tmp) {
+    while ( *tmp) {
+        bzero(ascii, 255);
         len = nonRepeatedLen(tmp);
         if ( len > longest )
             longest = len;
         tmp++;
     }
+    free(ascii);
     return longest;
 }
 
