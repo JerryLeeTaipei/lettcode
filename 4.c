dad74a964a0a7914b90345481b0b0b72a240d802
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-09 14:12:03 +0800

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,9 @@
+/*
+The Median is the "middle" of a sorted list of numbers.
+To find the Median, place the numbers in value order and find the middle.
+BUT, with an even amount of numbers , we find the middle pair of numbers, and then find the value that is half way between them.
+
+3, 5, 7, 12, 13, 14, 21, 23, 23, 23, 23, 29, 40, 56
+
+the middle numbers are 21 and 23, the Median is 22.
+*/
