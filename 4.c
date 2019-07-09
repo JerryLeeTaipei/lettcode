27576f675de9cb48d56e07002c33120a1f4a2a73
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-09 14:43:08 +0800

    Update 4.c

--- 4.c
+++ 4.c
@@ -7,3 +7,17 @@ BUT, with an even amount of numbers , we find the middle pair of numbers, and th
 
 the middle numbers are 21 and 23, the Median is 22.
 */
+
+double mergeSubBlocks(int *array, int* nums1, int nums1Size, int* nums2, int nums2Size){
+
+    // decompose until no overlapped or 1 element remained
+
+}
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
+    double median_value=0;  
+    array = malloc ( (nums1Size + nums2Size) * sizeof(int) );
+    mergeSubBlocks(array, nums1, nums1Size, nums2, nums2Size);
+    median_value = find_median(array);
+    return median_value;
+}
