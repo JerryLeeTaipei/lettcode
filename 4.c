7d0dd712d5c1e8cff6d55d48d6bad306da7da065
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-11 17:54:28 +0800

    Update 4.c

--- 4.c
+++ 4.c
@@ -8,14 +8,19 @@ BUT, with an even amount of numbers , we find the middle pair of numbers, and th
 the middle numbers are 21 and 23, the Median is 22.
 */
 
-double mergeSubBlocks(int *array, int* nums1, int nums1Size, int* nums2, int nums2Size){
+merge( int *dest, int *src1, int start_1, int end_1, int *src2, int start_2, int end_2) {
+    
+}
+    
+
+double mergeSubBlocks( int *dest, int *src1, int start_1, int end_1, int *src2, int start_2, int end_2){
     int mid=0;
     int *blk1_l=NULL, *blk1_r=NULL, *blk2_l=NULL, *blk2_r=NULL;
     // if non-overlapped
-    if ( nums1[num1Size-1] < nums2[0] ) {
-        merge(nums1, nums1Size, nums2, nums2Size);
-    } else if ( nums2[num2Size-1] < nums1[0] ) {
-        merge(nums2, nums2Size, nums1, nums1Size);
+    if ( src1[end_1] < src2[0] ) {
+        merge(array, src1, 0, end_1, src2, 0, end_2);
+    } else if ( src2[end_2] < src1[0] ) {
+        merge(array, src2, 0, end_2, src1, 0, end_1);
     } else { // decompose until no overlapped or 1 element remained
         
     }
@@ -24,7 +29,7 @@ double mergeSubBlocks(int *array, int* nums1, int nums1Size, int* nums2, int num
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
     double median_value=0;  
     array = malloc ( (nums1Size + nums2Size) * sizeof(int) );
-    mergeSubBlocks(array, nums1, nums1Size, nums2, nums2Size);
+    mergeSubBlocks(array, nums1, 0, nums1Size-1, nums2, 0, nums2Size-1);
     median_value = find_median(array);
     return median_value;
 }
