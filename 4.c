06d23156514311083ed72ab98236dcb29771a3be
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-11 21:17:35 +0800

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,28 +1,18 @@
+
 /*
 The Median is the "middle" of a sorted list of numbers.
 To find the Median, place the numbers in value order and find the middle.
 BUT, with an even amount of numbers , we find the middle pair of numbers, and then find the value that is half way between them.
-
 3, 5, 7, 12, 13, 14, 21, 23, 23, 23, 23, 29, 40, 56
-
 the middle numbers are 21 and 23, the Median is 22.
 */
 
-merge( int *dest, int *src1, int start_1, int end_1, int *src2, int start_2, int end_2) {
-    
-}
 
-/*
-    These 2 sorted arrays are non-overlapped
-    Eacg array is divided into 2 sub-blocks.
-    Compare and merge from the smaller(left) block, then, do the same for the bigger(right) block
-    Return the accumulated length of the merged blocks
-*/
 
 /*
     The left block is smaller than the right one
 */
-merge(int *array, int offset, int *src1, int start_1, int end_1, int *src2, int start_2, int end_2) {
+void merge(int *array, int offset, int *src1, int start_1, int end_1, int *src2, int start_2, int end_2) {
     int i=0, len=0;
     len = end_1 - start_1 + 1;
     for ( i=0; i< len; i++)
@@ -31,8 +21,16 @@ merge(int *array, int offset, int *src1, int start_1, int end_1, int *src2, int 
     for ( i=0; i< len; i++)
         array[offset +i] = src2[start_2 + i];
 }
-    
-int double mergeSort( int *array, int offset, int *src1, int start_1, int end_1, int *src2, int start_2, int end_2){
+ 
+
+/*
+    These 2 sorted arrays are non-overlapped
+    Eacg array is divided into 2 sub-blocks.
+    Compare and merge from the smaller(left) block, then, do the same for the bigger(right) block
+    Return the accumulated length of the merged blocks
+*/
+
+int  mergeSort( int *array, int offset, int *src1, int start_1, int end_1, int *src2, int start_2, int end_2){
     int mid1=0, mid2=0, len=0;
     
     len = (end_1 - start_1 + 1) + (end_2 - start_2 + 1);
@@ -52,13 +50,24 @@ int double mergeSort( int *array, int offset, int *src1, int start_1, int end_1,
     // compare the right block    
     offset = len;
     len = mergeSort(array, offset, src1, mid1+1, end_1, src2, mid2+1, end_2);
-    return len;
+    return (len+1);
+}
+
+double  find_median(int *array, int len){
+    double mid=0;
+    
+    if ( (len%2) == 1)
+        mid = array[(len/2) + 1];
+    else
+        mid = ( array[(len/2)]  + array[(len/2)+1] )/ 2;
+    return mid;
 }
 
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
+    int *array=NULL, len=0;
     double median_value=0;  
     array = malloc ( (nums1Size + nums2Size) * sizeof(int) );
-    mergeSort(array, 0, nums1, 0, nums1Size-1, nums2, 0, nums2Size-1);
-    median_value = find_median(array);
+    len = mergeSort(array, 0, nums1, 0, nums1Size-1, nums2, 0, nums2Size-1);
+    median_value = find_median(array, len);
     return median_value;
 }
