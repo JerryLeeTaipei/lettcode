87912c0589973e6f58d9a7800a779a8cad918c34
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-12 18:08:41 +0800

    Update 4.c

--- 4.c
+++ 4.c
@@ -12,15 +12,18 @@ the middle numbers are 21 and 23, the Median is 22.
 
 /*
     The left block is smaller than the right one
+    Update: offset 
 */
-void merge(int *array, int offset, int *src1, int start_1, int end_1, int *src2, int start_2, int end_2) {
+merge(int *array, int *offset, int *src1, int start_1, int end_1, int *src2, int start_2, int end_2) {
     int i=0, len=0;
-    len = end_1 - start_1 + 1;
+    len = end_1 - start_1 + 1;    
     for ( i=0; i< len; i++)
         array[offset +i] = src1[start_1 + i];
+    *offset = *offset + len;
     len = end_2 - start_2 + 1;
     for ( i=0; i< len; i++)
-        array[offset +i] = src2[start_2 + i];
+        array[offset +i] = src2[start_2 + i];        
+    *offset = *offset + len;
 }
  
 
@@ -31,49 +34,48 @@ void merge(int *array, int offset, int *src1, int start_1, int end_1, int *src2,
     Return the accumulated length of the merged blocks
 */
 
-int  mergeSort( int *array, int offset, int *src1, int start_1, int end_1, int *src2, int start_2, int end_2){
+void  mergeSort( int *array, int *offset, int *src1, int *start_1, int *end_1, int *src2, int *start_2, int *end_2){
     int mid1=0, mid2=0, len=0;
-    
-    len = (end_1 - start_1 + 1) + (end_2 - start_2 + 1);
-    
+        
     // if non-overlapped
-    if ( src1[end_1] < src2[0] ) {
-        merge(array, offset, src1, start_1, end_1, src2, start_2, end_2);
-        return (offset + len);
+    if ( src1[*end_1] < src2[*start_2] ) {
+        merge(array, *offset, src1, *start_1, *end_1, src2, *start_2, *end_2);
+        return;
     } else if ( src2[end_2] < src1[0] ) {
-        merge(array, offset,  src2, 0, end_2, src1, 0, end_1);   
-        return (offset + len);        
+        merge(array, *offset, src2, *start_2, *end_2, src1, *start_1, *end_1);   
+        return;
     }
    
-    // decompose until no overlapped or 1 element remained
-    mid1 = (end_1 - start_1)/2;
-    mid2 = (end_2 - start_2)/2;
-    // compare the left block
-    len = mergeSort(array, offset, src1, start_1, mid1, src2, start_2, mid2);
-    // compare the right block    
-    offset = len;
-    len = mergeSort(array, offset, src1, mid1+1, end_1, src2, mid2+1, end_2);
-  
-    return (len+1);
+    // decompose for at least 2 elements
+    if ( (*end_1 - *start_1) > 0 )
+        *end_1 = (*end_1 - *start_1)/2;
+    if ( (*end_2 - *start_q) > 0 )
+        *end_2 = (*end_2 - *start_2)/2;
+    
+    // merge the left block
+    mergeSort(array, offset, src1, start_1, end_1, src2, start_2, end_2);
+    // merge the right block    
+    mergeSort(array, offset, src1, mid1+1, end_1, src2, mid2+1, end_2);
 }
 
 double  find_median(int *array, int len){
     double mid=0;
     
     if ( (len%2) == 1)
-        mid = (double) array[(len/2) + 1];
+        mid = (double) array[(len/2)];
     else
-        mid = ( ((double) array[(len/2)])  + ((double) array[(len/2)+1]) ) / 2;
+        mid = ( (double) array[(len-1)/2]  +  (double) array[((len-1)/2)+1] ) / 2;
     return mid;
 }
 
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
-    int *array=NULL, len=0;
+    int *array=NULL, offset=0, start_1=0, end_1=0, start_2=0, end_2=0;
     double median_value=0;  
-    array = malloc ( (nums1Size + nums2Size) * sizeof(int) );
-//    len = mergeSort(array, 0, nums1, 0, nums1Size-1, nums2, 0, nums2Size-1);
-    len = 3; array[0] = 1; array[1] = 2; array[2] = 3;
-    median_value = find_median(array, len);
+    array = malloc ( (nums1Size + nums2Size) * sizeof(int) + 1); // add one 0 at the end
+    end_1 = nums1Size - 1;
+    end_2 = nums2Size - 1;
+    mergeSort(array, &offset, nums1, &start_1, &end_1, nums2, &start_2, &end_2);    
+    median_value = find_median(array, offset-1); // remove the 0 tail
     free(array);
     return median_value;
 }
