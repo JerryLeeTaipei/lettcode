a59d8df378ebf688f70a11cb098c3eb7d8f13899
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-17 18:23:37 +0800

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,53 +1,80 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-/*
-	palindrome:
-	
-	a word or group of words that is the same when you read it forwards from the beginning or backwards from the end:
-	
-	"aba", "ccc", "abcdcba"
-	
-	Both looking from the left to right and right to the left are the same.
-*/
 
+
+
+char *find_maxSymmetry(char *s, char *ptr, int *symmetry_len) {
+    char * turning_ptr=NULL, *l_ptr=NULL, *r_ptr=NULL;
+    int len = 1;
+
+    l_ptr = ptr;
+    if ( (*(ptr+1) != 0) &&  (*(ptr+2) != 0) ) { // test odd symmetric
+	printf("test odd sym\n");
+        if ( *(ptr) == *(ptr+2) ) {
+            turning_ptr = ptr +1;
+            r_ptr = ptr+2;
+            len = 3;
+        }
+    } 
+    if ( turning_ptr == NULL ){ // test even symmetric
+	printf("test even sym\n");
+        if ( *(ptr) == *(ptr+1) ) {
+            r_ptr = ptr+1;
+            len = 2;
+        }
+    }
+
+    // watch out for the starting and ending
+    while ( len > 1 ) { // symmetry is found
+       if ( (l_ptr == s) || ( *r_ptr == 0) ) // touch the start or ending
+            break;
+       else {
+            l_ptr--;
+            r_ptr++;
+            if ( *(l_ptr) == *(r_ptr)  ) { // continuous symmetry
+                len++;
+            } else { // end of symmetry
+                // rollback
+                l_ptr++;
+                r_ptr--;
+                break;
+            }
+        }
+    }
+
+    *symmetry_len = len;
+    printf("%c has max. len-%d symmetry:\n", *ptr, len);
+    int i=0;
+    for (i=0; i< len; i++)
+        printf("%c ", l_ptr[i]);
+    printf("\n");
+    return l_ptr;
+}
 
 char * longestPalindrome(char * s){
-    int len=0, answer_len=1, tmp_answer_len=0, tested_len=0;
-    char *ptr=NULL, target=0, *tmp_ptr=NULL, *answer_ptr=NULL;
+    int len=0, tested_len=0, answer_len=1, tmp_answer_len=1;
+    char *answer_ptr=NULL, *ptr=NULL, *tmp_answer_ptr=NULL;
     
     len = strlen(s);
     answer_ptr = s;
     ptr = s;
     while ( *ptr != 0 ) {
-        target = *ptr;
-	// TODO: check if the target has ben checked before, ignore if it is done
-
         tested_len = len - (ptr - s); // tested_len included the target
-	printf("%d: search %c for len(%d)\n", answer_len, target,tested_len);
-        tmp_answer_len=1; // reset count for each position
-        tmp_ptr = ptr + 1;
-	if ( *(tmp_ptr + 1) == 0 ) // ignore the last char
+	printf("%d: find for remained len(%d)\n", answer_len, tested_len);
+	if ( *(ptr + 1) == 0 ) // ignore the last char
 		break;
-        while ( *tmp_ptr != 0 ) { // for each position
-	    printf("%c is %c?\n", *tmp_ptr, target);
-	    tmp_answer_len++;
-            if ( *tmp_ptr == target ) {
-	        if ( tmp_answer_len > answer_len ) {
-            	    answer_len = tmp_answer_len;
-            	    answer_ptr = ptr;
-	    	    printf("Change answer=%c with len(%d)\n", *answer_ptr, answer_len);
-		}
-		// reset counting for this target
-		tmp_answer_len = 1;
-            }
-            tmp_ptr++;
-        }// while	
-	if ( *tmp_ptr == 0 ) // no matched
-	       tmp_answer_len = 0;
-	printf("%c with max. len(%d)\n", *ptr, tmp_answer_len);    
-        ptr++;
+// main - begins
+        tmp_answer_ptr = find_maxSymmetry(s, ptr, &tmp_answer_len);
+        if ( tmp_answer_len > answer_len ) {
+            answer_len = tmp_answer_len;
+            answer_ptr = tmp_answer_ptr;
+        }
+// main - ends
+	printf("Current max. len(%d): %s\n", answer_len, answer_ptr);    
+        ptr++; // shift 1 for next test
 	if ( *ptr == 0 )
 		break;
     }// while
@@ -59,7 +86,7 @@ char * longestPalindrome(char * s){
 
 int main(){
 	char *answer=NULL;
-	answer = longestPalindrome("babad");
+	answer = longestPalindrome("aaaa");
 	if ( answer )
 		printf("%s\n", answer);
 	else
