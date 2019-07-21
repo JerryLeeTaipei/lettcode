8500c4c3868e88aa682b6244e65a62eb6f859571
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-21 10:24:40 +0800

    Update 6.c

--- 6.c
+++ 6.c
@@ -57,7 +57,86 @@ else
 
 */
 
-char * convert(char * s, int numRows){
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+char * convert(char * s, int numRows){
+  char *ptr=NULL;
+  int r=0, c=0, len=0, anchor=0, tail=0, i=0, a=0, index=0;
+
+  len = strlen(s);
+  anchor = len / numRows;
+  tail = len % numRows;
+
+  printf(" malloc(%d+1) for %d x %d - %d\n", len, numRows, anchor, (numRows - tail));
+  ptr = malloc( sizeof(char) * (len + 1));
+  ptr[len] = 0;
+  i = 0;
+  r = 1;
+  a = 0;
+  while ( i < len ) {
+        printf("---i(%d), r(%d), a(%d)\n", i, r, a);
+        if ( (r == 1) || ( r == numRows) ) {
+            if ( r == 1 )    
+                index = ( numRows - 1 ) * (a * 2);
+            else
+                index = ( numRows - 1 ) * (a * 2 + 1);
+            ptr[i] = s[index];
+            printf("r(%d),a(%d), ptr[%d]=%c <- s[%d]\n", r, a, i, ptr[i], index);
+            a++;
+            if ( a >= anchor ) {
+                a = 0;
+                r++;
+            }
+        } else {
+            index = ( numRows - 1 ) * (a * 2) + (r-1);
+            if ( index < len ) {
+                ptr[i] = s[index];
+                printf("1-r(%d),a(%d), ptr[%d]=%c <- s[%d]\n", r, a, i, ptr[i], index);
+                i++;
+                if ( i >= len ) {
+                    printf("i-OOR:%d\n", i);
+                    break;
+               }
+                index = ( numRows - 1 ) * (a * 2 + 1) + (numRows-r);
+                if ( index < len ) {
+                    ptr[i] = s[index];
+                    printf("2-r(%d),a(%d), ptr[%d]=%c <- s[%d]\n", r, a, i, ptr[i], index);
+                    a++;
+                    if ( a >= anchor ) {
+                        a = 0;
+                        r++;
+                    }
+                } else {
+                    printf("index-OOR:%d\n", index);
+                    i--;
+                    a = 0;
+                    r++;
+                }
+            } else {
+                printf("index-OOR:%d\n", index);
+                i--;
+                a = 0;
+                r++;
+            }
+        }
+        i++;
+  } // while
+
+endScan:  
+  return ptr;
+}
+
+
+int main(){
+  char *ptr=NULL;
+
+  ptr = convert("PAYPALISHIRING",3);
+
+  if ( ptr ) {
+    printf("%s\n", ptr);
+    free(ptr);
+  }
 }
 
