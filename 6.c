2f01d18e85fff3cff282805e8235b5b62ffc92cc
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-21 16:10:09 +0800

    Update 6.c

--- 6.c
+++ 6.c
@@ -63,29 +63,45 @@ else
 
 char * convert(char * s, int numRows){
   char *ptr=NULL;
-  int r=0, c=0, len=0, anchor=0, tail=0, i=0, a=0, index=0;
+  int r=0, c=0, len=0, anchors=0, tail=0, i=0, a=0, index=0;
 
-  len = strlen(s);
-  anchor = len / numRows;
-  tail = len % numRows;
+  if ( (s != NULL) && (*s != 0) && (numRows > 0) ) {
+      len = strlen(s);
+      if ( (len <= numRows) || (numRows <=1) ){
+        ptr = malloc( sizeof(char) * (len + 1));
+        ptr[len] = 0;
+        strncpy(ptr, s, len);
+        goto endScan;
+      }
+      c = len / numRows;
+      anchors = len/(2*numRows-2);
+      if ( (len % (2*numRows-2)) > 0 )
+        anchors++;
+      tail = len % numRows;
+  } else if ( *s == 0 ) {
+      ptr = malloc(sizeof(char));
+      *ptr = 0;
+      return ptr;
+  } else
+    return NULL;
 
-  printf(" malloc(%d+1) for %d x %d - %d\n", len, numRows, anchor, (numRows - tail));
+  printf(" malloc(%d+1) for %d x %d - %d, anchors=%d\n", len, numRows, c, (numRows - tail), anchors);
   ptr = malloc( sizeof(char) * (len + 1));
   ptr[len] = 0;
   i = 0;
   r = 1;
   a = 0;
   while ( i < len ) {
-        printf("---i(%d), r(%d), a(%d)\n", i, r, a);
+        //printf("---i(%d), r(%d), a(%d)\n", i, r, a);
         if ( (r == 1) || ( r == numRows) ) {
             if ( r == 1 )    
                 index = ( numRows - 1 ) * (a * 2);
             else
                 index = ( numRows - 1 ) * (a * 2 + 1);
             ptr[i] = s[index];
-            printf("r(%d),a(%d), ptr[%d]=%c <- s[%d]\n", r, a, i, ptr[i], index);
+            //printf("r(%d),a(%d), ptr[%d]=%c <- s[%d]\n", r, a, i, ptr[i], index);
             a++;
-            if ( a >= anchor ) {
+            if ( a >= anchors ) {
                 a = 0;
                 r++;
             }
@@ -93,29 +109,29 @@ char * convert(char * s, int numRows){
             index = ( numRows - 1 ) * (a * 2) + (r-1);
             if ( index < len ) {
                 ptr[i] = s[index];
-                printf("1-r(%d),a(%d), ptr[%d]=%c <- s[%d]\n", r, a, i, ptr[i], index);
+                //printf("1-r(%d),a(%d), ptr[%d]=%c <- s[%d]\n", r, a, i, ptr[i], index);
                 i++;
                 if ( i >= len ) {
-                    printf("i-OOR:%d\n", i);
+                    //printf("i-OOR:%d\n", i);
                     break;
                }
                 index = ( numRows - 1 ) * (a * 2 + 1) + (numRows-r);
                 if ( index < len ) {
                     ptr[i] = s[index];
-                    printf("2-r(%d),a(%d), ptr[%d]=%c <- s[%d]\n", r, a, i, ptr[i], index);
+                    //printf("2-r(%d),a(%d), ptr[%d]=%c <- s[%d]\n", r, a, i, ptr[i], index);
                     a++;
-                    if ( a >= anchor ) {
+                    if ( a >= anchors ) {
                         a = 0;
                         r++;
                     }
                 } else {
-                    printf("index-OOR:%d\n", index);
+                    //printf("index-OOR:%d\n", index);
                     i--;
                     a = 0;
                     r++;
                 }
             } else {
-                printf("index-OOR:%d\n", index);
+                //printf("index-OOR:%d\n", index);
                 i--;
                 a = 0;
                 r++;
@@ -124,7 +140,7 @@ char * convert(char * s, int numRows){
         i++;
   } // while
 
-endScan:  
+endScan:
   return ptr;
 }
 
