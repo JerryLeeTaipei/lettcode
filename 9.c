e1fe1d7e485f1fcc9d828fdb848de6525fbd66a2
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-24 11:42:57 +0800

    Update 9.c

--- 9.c
+++ 9.c
@@ -16,3 +16,86 @@ Input: 10
 Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_DIGITS    0xFFFFFFFF
+
+int max_digits(){
+    int digits=0;
+    long num = 0xFFFFFFFF - 1;
+    
+
+    digits = 1;
+    while ( num > 0 ) {
+        num = num/10;
+        digits++;
+        
+    }
+    return digits;
+}
+
+bool isPalindrome(int x){
+    bool ret=false;
+    int tmp = 0, i=0, j=0, noDigits=0, decimalDigits=0;
+    char *digits=NULL;
+ 
+    if ( x < 0 )
+        return false;
+    decimalDigits =  max_digits();
+    printf("max. digits=%d\n", decimalDigits);   
+    digits = malloc( sizeof (char) * decimalDigits );
+    if ( digits ) {
+        tmp = x;
+        i = 0;
+        while ( tmp > 9 ) {
+            digits[i] = tmp % 10;
+            i++;
+            tmp = tmp/10;
+            printf("tmp=%d\n", tmp);
+            if ( tmp < 10 ) {
+                digits[i] = tmp;
+                i++;
+                break;
+            }
+        }
+        noDigits = i;
+        printf("%d has %d digits\n", x, noDigits);
+        if ( noDigits < 2 ) {
+            ret = true;
+            goto free_m;
+        }
+        i = 0;
+        j = noDigits - 1;
+        ret = true;      
+        while ( i != j ) {
+            printf("%d : %d\n",  digits[i] , digits[j]);
+            if ( digits[i] != digits[j] ) {
+                ret = false;
+                break;
+            }
+            i++;
+            j--;
+            if ( j == 0 )
+                break;
+        }
+
+    }    
+free_m:
+    if (digits)
+        free(digits);
+    return ret;
+}
+
+
+int main(){
+    int num=-121;
+    
+    printf("%d(0x %x)\n", num, num);
+    if ( isPalindrome(num) )
+        printf("%d is Palindrome\n", num);
+    else
+        printf("%d is Not Palindrome\n", num);
+}
