0684ecb1077f5d5995a7e4aa2f761d56c1edddcf
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2019-07-29 11:16:07 +0800

    Update 11.c

--- 11.c
+++ 11.c
@@ -9,3 +9,34 @@ Input: [1,8,6,2,5,4,8,3,7]
 Output: 49
 
 */
+
+
+
+int maxArea(int* height, int heightSize){
+    int width=0, area=0, max_area=0, index=0, offset=0, h=0;
+    char *ptr=NULL;
+
+        
+    printf("heightSize=%d\n", heightSize);
+    width = heightSize-1;
+    while ( width >0 ) {
+        offset = width;
+        index = 0;
+        while ( offset < heightSize ) {
+            //printf("%d/%d\n", offset, width);
+            h = (height[index] < height[offset]) ? height[index] : height[offset];
+            area = width * h;
+            //printf("[%d, %d]:w(%d)*h(%d)=%d\n", index, offset, width, h, area);
+            if ( area >= max_area ) {
+                max_area = area; 
+                printf("max_area=%d\n", max_area);
+            } else {
+                printf("area=%d\n", area);
+            }
+            index++;
+            offset++;
+        }       
+        width--;
+    }
+    return max_area;
+}
