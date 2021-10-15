78b17309aefa8c51c0cf3de65ae643c8e49f8868
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2021-10-15 20:33:59 +0800

    test.cpp, ex2

--- test.cpp
+++ test.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <cstring>
+
+int add(int x, int y){
+
+    return(x+y);
+}
+
+char *cat_str(const char *c1, const char *c2){
+    char *ptr = NULL;
+
+    ptr = (char *) malloc(strlen(c1)+strlen(c2)+1);
+    strncpy(ptr, c1, strlen(c1) );
+    strncpy(ptr+ strlen(c2), c2, strlen(c2) );
+    ptr[strlen(c1)+strlen(c2)] = 0;
+    return ptr;
+}
+
 int main()
 {
-std::cout << "Solidum petit in profundis!\n";
-return 0;
+    char *ptr=NULL;
+    ptr = cat_str("Hello", "World");
+    std::cout << "2 + 3 = " << add(2, 3) << "\n";
+    std::cout << ptr << "\n";
+    free(ptr);
+    
+    return 0;
 }
