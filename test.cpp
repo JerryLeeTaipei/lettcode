a9fd04403b7aa0bcdd297650f15d3a87de681a4f
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2021-11-07 15:08:58 +0800

    stack

--- test.cpp
+++ test.cpp
@@ -1,22 +1,43 @@
-// C++ program to demonstrate functionality of unordered_map
+//simple program that checks for missing parantheses
 #include <iostream>
-#include <unordered_map>
+#include <stack>
+#include <string>
+
 using namespace std;
- 
-int main()
-{
-    // Declaring umap to be of <string, int> type
-    // key will be of string type and mapped value will
-    // be of int type
-    unordered_map<string, int> umap;
- 
-    // inserting values by using [] operator
-    umap["GeeksforGeeks"] = 10;
-    umap["Practice"] = 20;
-    umap["Contribute"] = 30;
- 
-    // Traversing an unordered map
-    for (auto x : umap)
-      cout << x.first << " = " << x.second << endl;
- 
+// returns whether the parentheses in the input are balanced
+bool parChecker(string symbolString) {
+    stack<string> s;
+    bool balanced = true;
+    int index = 0;
+    int str_len = symbolString.length();
+
+    while (index < str_len && balanced) {
+              string symbol;
+              symbol = symbolString[index];
+              if (symbol == "(") {
+                  s.push(symbol); //pushes the open parentheses to the stack.
+              } else {
+                  if (s.empty()) { //if there is no open parentheses in the stack,
+                                   //the closing parentheses just found makes the string unbalanced.
+                        balanced = false;
+                  } else { //if there is an open parentheses in the stack,
+                           //the closing parentheses just found has a matching open parentheses.
+                       s.pop();
+                  }
+              }
+    index = index + 1;
+    }
+
+    if (balanced && s.empty()) { //if the string is balanced and there are no
+                                 //remaining open parentheses.
+        return true;
+    } else {
+        return false;
+    }
 }
+
+int main() {
+    cout << parChecker("((()))") << endl;
+    cout << parChecker("(()") << endl;
+}
+
