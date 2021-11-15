62d67ecaf146e410d4c5ba7949197b647d262ec5
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2021-11-15 08:07:03 +0800

    bubble sort using reference array

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 //function goes through list sorting adjacent values as it bubbles
 //the largest value to the top.
-vector<int> bubbleSort(vector<int> avector) { //the vector for bubble sort
+void bubbleSort(vector<int> &avector) { //the vector for bubble sort
   for (int passnum = avector.size()-1; passnum > 0; passnum -= 1) {
       for (int i = 0; i < passnum; i++) {
           if (avector[i] > avector[i+1]) {
@@ -14,24 +14,23 @@ vector<int> bubbleSort(vector<int> avector) { //the vector for bubble sort
           }
       }
   }
-  return avector;
+  return ;
 }
 
 int main() {
     // Vector initialized using a static array
     static const int arr[] = {54,26,93,17,77,31,44,55,20};
     vector<int> avector (arr, arr + sizeof(arr) / sizeof(arr[0]) );
-
-    vector<int> bvector = bubbleSort(avector);
+    for (unsigned int i = 0; i < avector.size(); i++) {
+        cout << avector[i] << " ";
+    }
+    cout << endl;
+    bubbleSort(avector);
 
     for (unsigned int i = 0; i < avector.size(); i++) {
         cout << avector[i] << " ";
     }
     cout << endl;
-    for (unsigned int i = 0; i < bvector.size(); i++) {
-        cout << bvector[i] << " ";
-    }
-    cout << endl;
+
     return 0;
 }
-
