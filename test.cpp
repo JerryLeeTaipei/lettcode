e189f5fbc880fc17be3014e570ae51ab86a9bed5
JerryLeeTaipei <38965091+JerryLeeTaipei@users.noreply.github.com>
2021-12-08 10:31:23 +0800

    73. Set Matrix Zeroes

--- test.cpp
+++ test.cpp
@@ -1,195 +1,75 @@
-#include <fstream>
 #include <iostream>
-#include <map>
-#include <string>
 #include <vector>
+
 using namespace std;
 
-class Vertex {
+
+class Solution {
 public:
-      string id;
-      map<string, float> connectedTo;
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<bool> r0, c0;
+        int i=0, j=0;
+        //cout << matrix.size() << " x " << matrix[0].size() << endl;
 
-      Vertex() {
-      }
-
-      Vertex(string key) {
-              id = key;
-      }
-
-      void addNeighbor(string nbr, float weight = 1) {
-              connectedTo[nbr] = weight;
-      }
-
-      vector<string> getConnections() {
-              vector<string> keys;
-              // Use of iterator to find all keys
-              for (map<string, float>::iterator it = connectedTo.begin();
-                       it != connectedTo.end();
-                       ++it) {
-                      keys.push_back(it->first);
-              }
-              return keys;
-      }
-
-      string getId() {
-              return id;
-      }
-
-      float getWeight(string nbr) {
-              return connectedTo[nbr];
-      }
-
-      friend ostream &operator<<(ostream &, Vertex &);
-};
-
-ostream &operator<<(ostream &stream, Vertex &vert) {
-      vector<string> connects = vert.getConnections();
-      stream << vert.id << " -> ";
-      for (unsigned int i = 0; i < connects.size(); i++) {
-              //stream << connects[i] << endl << "\t";
-              stream << connects[i] << "\t";
-      }
-      return stream;
-}
-
-class Graph {
-public:
-      map<string, Vertex> vertList;
-      int numVertices;
-      bool directional;
-
-      Graph(bool directed = true) {
-              directional = directed;
-              numVertices = 0;
-      }
-
-      Vertex addVertex(string key) {
-              numVertices++;
-              Vertex newVertex = Vertex(key);
-              this->vertList[key] = newVertex;
-              return newVertex;
-      }
-
-      Vertex *getVertex(string n) {
-              for (map<string, Vertex>::iterator it = vertList.begin();
-                       it != vertList.end();
-                       ++it) {
-                      if (it->first == n) {
-                              // Forced to use pntr due to possibility of returning NULL
-                              Vertex *vpntr = &vertList[n];
-                              return vpntr;
-                      } else {
-                              return NULL;
-                      }
-              }
-      }
-
-      bool contains(string n) {
-              for (map<string, Vertex>::iterator it = vertList.begin();
-                       it != vertList.end();
-                       ++it) {
-                      if (it->first == n) {
-                              return true;
-                      }
-              }
-              return false;
-      }
-
-      void addEdge(string f, string t, float cost = 1) {
-              if (!this->contains(f)) {
-                      this->addVertex(f);
-              }
-              if (!this->contains(t)) {
-                      this->addVertex(t);
-              }
-              vertList[f].addNeighbor(t, cost);
-
-              if (!directional) {
-                      vertList[t].addNeighbor(f, cost);
-              }
-      }
-
-      vector<string> getVertices() {
-              vector<string> verts;
-
-              for (map<string, Vertex>::iterator it = vertList.begin();
-                       it != vertList.end();
-                       ++it) {
-                      verts.push_back(it->first);
-              }
-              return verts;
-      }
-
-      friend ostream &operator<<(ostream &, Graph &);
-};
-
-ostream &operator<<(ostream &stream, Graph &grph) {
-      cout << "= Display the graph =\n";
-      for (map<string, Vertex>::iterator it = grph.vertList.begin();
-               it != grph.vertList.end();
-               ++it) {
-              stream << it->second;
-        cout<<endl;
-      }
-
-      return stream;
-}
-
-string getBlank(string str, int index) {
-      string blank = str;
-      blank[index] = '_';
-      return blank;
-}
-
-Graph buildGraph(vector<string> words) {
-      Graph g(false);
-
-      map<string, vector<string> > bucket_map_vec;
-
-      // Go through the words to create buckets
-      cout << "= Create buckets =\n";
-      for (unsigned int i = 0; i < words.size(); i++) {
-              // Go through each letter, making it blank
-                cout << words[i] << ": ";
-              for (unsigned int j = 0; j < words[i].length(); j++) {
-                      string bucket = getBlank(words[i], j);
-                        cout << "\t" << bucket;
-                      // Add the word to the map at the location of the blank
-                      bucket_map_vec[bucket].push_back(words[i]);
-              }
-            cout << endl;
-      }
-
-      // go through each bucket to add edges in the same bucket
-      cout << "= Create edges =\n";
-      for (map<string, vector<string> >::iterator iter = bucket_map_vec.begin(); iter != bucket_map_vec.end(); ++iter) {
-        if ( iter->second.size() < 2 ) // the edge between 2 vertexes
-            continue;
-        cout << iter->first << ": " ;
-        for(unsigned int i=0; i<iter->second.size();i++) {
-            for (unsigned int j=0; j<iter->second.size();j++) {
-                if (iter->second[i]!=iter->second[j]) {
-                    cout << iter->second[i] << "-" << iter->second[j] << "\t";
-                    g.addEdge(iter->second[i],iter->second[j]);
+        for ( i =0; i < matrix.size(); i++ )
+            r0.push_back(0); // reset for each row's flag
+        for ( i =0; i < matrix[0].size(); i++ )
+            c0.push_back(0); // reset for each column's flag
+                
+        for (i=0; i < r0.size(); i++) {
+            for ( j=0; j < c0.size(); j++) {
+                //cout << "Test: " <<i << "," << j << "=" << matrix[i][j] << endl;
+                if ( matrix[i][j] == 0 ) {
+                    //cout << "Set: " <<i << "," << j << endl;
+                    c0[j] = 1;
+                    r0[i] = 1;                    
                 }
+
             }
         }
-        cout << endl;
-      }
+        //cout << "change output\n";
+        for (i=0; i < r0.size(); i++) {
+            //cout << "i=" << i << "-->" << endl;
+            vector<int> &r=matrix[i];
+            for ( j=0; j < r.size(); j++) {
+                //cout << i << "," << j << endl;
+                if ( r0[i] == 1 ) {
+                    //cout << "Clear row#" << i << endl;
+                    r[j] = 0;
+                } else {
+                    if ( c0[j] == 1){
+                        //cout << "Clear column#" << j << endl;
+                        r[j] = 0;
+                    }
+                }
+                //cout << "<--j=" << j << endl;
+            }
+            //cout << "<--i=" << i << endl;
+        }
+        return;
+    }
+};
 
-    return g;
+int main(){
+    Solution test;
+    vector<vector<int>> matrix;
+
+    int r1[]={0,1};
+    vector<int> v1(r1,r1+2);
+    matrix.push_back(v1);
+#if 0
+    int r1[]={0,1,2,0},r2[]={3,4,5,2},r3[]={1,3,1,5};
+
+    vector<int> v1(r1,r1+4);
+    vector<int> v2(r2,r2+4);
+    vector<int> v3(r3,r3+4);
+    
+    matrix.push_back(v1);
+    matrix.push_back(v2);
+    matrix.push_back(v3);
+#endif
+
+
+    test.setZeroes(matrix) ;   //1836311903
+    return 0;
 }
-
-int main() {
-    // Vector Initialized with an array
-    string arr[] = {"fool","cool","pool","poll","pole","pall","fall","fail","foil","foul","pope","pale","sale","sage","page"};
-    vector<string> words(arr,arr+(sizeof(arr)/sizeof(arr[0])));
-
-      Graph g = buildGraph(words);
-
-      cout << g << endl;
-
-      return 0;
-}
-
